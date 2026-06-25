36089d2a89082f69352bed72b2b171fe06ec33c8
DIVYAASHRE22 <divyaashregayathri@gmail.com>
2026-06-25 16:35:02 +0530

    Add files via upload

---
+++ Week 1/Design Patterns and Principles/SingletonPattern.java
@@ -0,0 +1,30 @@
+public class SingletonPattern{
+	static class Logger{
+		public static Logger instance;
+		public Logger(){
+			System.out.println("Logger instance created.");
+		}
+		public static Logger getInstance(){
+			if(instance == null){
+				instance = new Logger();
+			}
+			return instance;
+		}
+		public void log(String message){
+			System.out.println("Log : " +message);
+		}
+	}
+	public static void main(String args[]){
+		Logger logger1 = Logger.getInstance();
+		Logger logger2 = Logger.getInstance();
+		logger1.log("Application Started");
+		logger2.log("Processing Data");
+		if(logger1 == logger2){
+			System.out.println("Only one Logger instance exists.");
+		}
+		else{
+			System.out.println("Multiple Logger instance exists.");
+		}
+	}
+}
+		
